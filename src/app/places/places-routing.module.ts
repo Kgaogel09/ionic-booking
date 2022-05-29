37619e652495c04d996dbd1959adb17adada4a19
Kgaogel09 <kgaogelo@swipeix.com>
2022-05-29 21:46:47 +0200

    added tabs

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -1,3 +1,4 @@
+import { EditOfferPageModule } from './offers/edit-offer/edit-offer.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -5,17 +6,71 @@ import { PlacesPage } from './places.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      {
+        path: 'discover',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('./discover/discover.module').then(
+                (m) => m.DiscoverPageModule
+              ),
+          },
+          {
+            path: ':placeId',
+            loadChildren: () =>
+              import('./discover/place-details/place-details.module').then(
+                (m) => m.PlaceDetailsPageModule
+              ),
+          },
+        ],
+      },
+      {
+        path: 'offers',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('./offers/offers.module').then((m) => m.OffersPageModule),
+          },
+          {
+            path: 'new',
+            loadChildren: () =>
+              import('./offers/new-offer/new-offer.module').then(
+                (m) => m.NewOfferPageModule
+              ),
+          },
+          {
+            path: 'edit/:placeId',
+            loadChildren: () =>
+              import('./offers/edit-offer/edit-offer.module').then(
+                (m) => m.EditOfferPageModule
+              ),
+          },
+          {
+            path: ':placeId',
+            loadChildren: () =>
+              import('./offers/offer-bookings/offer-bookings.module').then(
+                (m) => m.OfferBookingsPageModule
+              ),
+          },
+        ],
+      },
+      {
+        path: '',
+        redirectTo: '/places/tabs/discover',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
     path: '',
-    component: PlacesPage
+    redirectTo: '/places/tabs/discover',
+    pathMatch: 'full',
   },
-  {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'offers',
-    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
-  }
 ];
 
 @NgModule({
