7a7d2e880b637dacb70bbb2f26f9a88cc186d086
Kgaogel09 <kgaogelo@swipeix.com>
2022-05-30 13:40:38 +0200

    edit offers page

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-edit-offer',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-offer.page.scss'],
 })
 export class EditOfferPage implements OnInit {
+  loadedOffer: Place;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private placeService: PlacesService,
+    private router: Router,
+    private navCtrl: NavController
+  ) {}
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has('placeId')) {
+        //redirect
+        this.navCtrl.navigateBack(['/places/tabs/discover']);
+        return;
+      }
+      const placeId = paramMap.get('placeId');
+      this.loadedOffer = this.placeService.getPlace(placeId);
+    });
   }
-
 }
