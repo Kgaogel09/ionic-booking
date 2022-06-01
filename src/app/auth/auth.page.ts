6bf2450faab254d8ee585e8ef6bc008e2ed0e1e3
Kgaogel09 <kgaogelo@swipeix.com>
2022-06-01 11:09:42 +0200

    created loading controller

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,6 +1,7 @@
 import { AuthService } from './auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-auth',
@@ -8,14 +9,31 @@ import { Router } from '@angular/router';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
-  login = false;
-  constructor(private authService: AuthService, private route: Router) {}
+  isLoading = false;
+  constructor(
+    private authService: AuthService,
+    private route: Router,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ngOnInit() {}
 
   onLogin() {
     this.authService.login();
-    setTimeout(() => {}, 2000);
-    this.route.navigateByUrl('/places/tabs/discover');
+    this.isLoading = true;
+    this.loadingCtrl
+      .create({
+        keyboardClose: true,
+        spinner: 'bubbles',
+        message: 'loading...',
+      })
+      .then((loadingEl) => {
+        loadingEl.present();
+        setTimeout(() => {
+          this.isLoading = false;
+          loadingEl.dismiss();
+          this.route.navigateByUrl('/places/tabs/discover');
+        }, 2000);
+      });
   }
 }
