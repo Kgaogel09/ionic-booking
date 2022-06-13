66ed69f8f1a0615740a35294ee922831b85a3908
Kgaogel09 <kgaogelo@swipeix.com>
2022-06-13 16:29:29 +0200

    cancel booking

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,5 +1,5 @@
 import { Subscription } from 'rxjs';
-import { IonItemSliding } from '@ionic/angular';
+import { IonItemSliding, LoadingController } from '@ionic/angular';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Booking } from './bookings.model';
 import { BookingsService } from './bookings.service';
@@ -13,7 +13,10 @@ export class BookingsPage implements OnInit, OnDestroy {
   loadedBookings: Booking[];
   private bookingSub: Subscription;
 
-  constructor(private bookingService: BookingsService) {}
+  constructor(
+    private bookingService: BookingsService,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ngOnInit() {
     // this.loadedBookings = this.bookingService.bookings;
@@ -33,5 +36,15 @@ export class BookingsPage implements OnInit, OnDestroy {
     //   (booking) => booking.id !== bookingId
     // );
     slidingEl.close();
+    this.loadingCtrl
+      .create({
+        message: 'Deleting booking...',
+      })
+      .then((loadingEl) => {
+        loadingEl.present();
+        this.bookingService.cancelBooking(bookingId).subscribe(() => {
+          loadingEl.dismiss();
+        });
+      });
   }
 }
--- src/app/bookings/bookings.service.ts
+++ src/app/bookings/bookings.service.ts
@@ -51,5 +51,15 @@ export class BookingsService {
     );
   }
 
-  cancelBooking(bookingId: string) {}
+  cancelBooking(bookingId: string) {
+    return this.bookings.pipe(
+      take(1),
+      delay(1000),
+      tap((bookings) => {
+        this._bookings.next(
+          bookings.filter((booking) => booking.id !== bookingId)
+        );
+      })
+    );
+  }
 }
